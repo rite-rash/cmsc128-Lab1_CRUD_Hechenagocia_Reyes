a3fbb13a73a19c64f5d4750d10551195fccf6b8b
Trisha Mae A. Hechenagocia <tahechenagocia@up.edu.ph>
2026-09-05 21:00:00 +0800

    build: add prettier plugin for tailwind

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,5 @@
+// DESCRIPTION: import global styles in tailwind and pull primary UI component tree  in App
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
